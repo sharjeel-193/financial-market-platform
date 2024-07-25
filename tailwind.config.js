0a67b1f9265e7fbf69e74dd56eb06b3457f31b66
Muhammad Sharjeel Maqsood <m.sharjeel193@gmail.com>
2024-07-25 21:23:41 +0300

    Initial Setup of the App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    darkMode: 'class',
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#13b9b9',
+                    // light: '#3bcfcf',
+                    // dark: '#0f9494',
+                },
+                secondary: {
+                    DEFAULT: '#ffa500',
+                    // light: '#ffb733',
+                    // dark: '#cc8400',
+                },
+                background: {
+                    light: '#ffffff', // Light background
+                    dark: '#1a1a1a',  // Dark background
+                },
+                text: {
+                light: '#000000', // Light text
+                dark: '#ffffff',  // Dark text
+                },
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
